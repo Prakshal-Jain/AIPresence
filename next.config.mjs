8f9be61988d5aa2a7b253a1d812be792f7694061
Prakshal Jain <prakshal@buffalo.edu>
2025-10-21 01:28:55 -0700

    fixed basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
-
 const nextConfig = {
   output: 'export',
-  basePath: basePath,
-  assetPrefix: basePath,
   typescript: {
     ignoreBuildErrors: true,
   },
